2fa98d289eb4fc86a11f812323d839f41f2a6664
TheAkash2002 <mahapatraakash@gmail.com>
2021-04-28 10:47:53 +0530

    Initial setup

---
+++ app/src/main/java/com/hackfest21/covigenix/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hackfest21.covigenix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
